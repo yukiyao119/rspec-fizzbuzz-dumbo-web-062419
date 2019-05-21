d4774188038d337de3d018f0b5d37664e5d47499
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-21 23:10:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif (int % 3 == 0) && (int % 5 == 0)
+  elsif (int % 3 == 0) and (int % 5 == 0)
     return "FizzBuzz"
   else
     return nil
