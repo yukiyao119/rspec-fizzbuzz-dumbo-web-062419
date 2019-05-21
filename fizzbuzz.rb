bbf1ce77d45d018b92ba83b6f86bb8f020779110
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-21 23:06:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif (int % 15 == 0)
-    puts "FizzBuzz"
+    return "FizzBuzz"
   else
-    puts nil
+    return nil
   end
 end
